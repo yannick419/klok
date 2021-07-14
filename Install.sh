c78a9289b3eeaac2a916a26c4b5698dbf7610fd7
yannick419 <87373685+yannick419@users.noreply.github.com>
2021-07-14 22:04:08 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 # Install alarmclock settings
 # Yannick Lambregts - 14/07/2021
-echo
-echo updating git
-git pull
-echo ------
+
 #echo
 #echo make scripts executable
 #find -type f -iname "*.sh" -exec chmod +x {} \;
 #echo ------
+
 echo
 echo installing mplayer
 sudo apt-get install mplayer
 echo ------
+
 echo
-echo updating cron
-./UpdateCron.sh
+echo update
+./Update.sh
 echo ------
-echo
-echo updating playlists
-./UpdatePlaylists.sh
-echo ------
+
 echo
 echo Dit sluit binnen 100 seconden
 sleep 100
