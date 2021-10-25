8b70e0e4e1208852874625ec26793a79b7e26b83
yannick419 <87373685+yannick419@users.noreply.github.com>
2021-10-25 21:18:59 +0200

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,59 @@
+import os,signal
+import RPi.GPIO as GPIO
+
+print("""buttons.py - Detect which button has been pressed
+
+This example should demonstrate how to:
+1. set up RPi.GPIO to read buttons,
+2. determine which button has been pressed
+
+Press Ctrl+C to exit!
+
+""")
+
+# The buttons on Pirate Audio are connected to pins 5, 6, 16 and 24
+# Boards prior to 23 January 2020 used 5, 6, 16 and 20 
+# try changing 24 to 20 if your Y button doesn't work.
+BUTTONS = [5, 6, 16, 24]
+
+# These correspond to buttons A, B, X and Y respectively
+LABELS = ['A', 'B', 'X', 'Y']
+
+# Set up RPi.GPIO with the "BCM" numbering scheme
+GPIO.setmode(GPIO.BCM)
+
+# Buttons connect to ground when pressed, so we should set them up
+# with a "PULL UP", which weakly pulls the input signal to 3.3V.
+GPIO.setup(BUTTONS, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+# "handle_button" will be called every time a button is pressed
+# It receives one argument: the associated input pin.
+def handle_button(pin):
+    label = LABELS[BUTTONS.index(pin)]
+    print("Button press detected on pin: {} label: {}".format(pin, label))
+    try:
+         
+        # iterating through each instance of the process
+        for line in os.popen("ps ax | grep mplayer | grep -v grep"):
+            fields = line.split()
+             
+            # extracting Process ID from the output
+            pid = fields[0]
+             
+            # terminating process
+            os.kill(int(pid), signal.SIGKILL)
+        print("Process Successfully terminated")
+         
+    except:
+        print("Error Encountered while running script")
+
+# Loop through out buttons and attach the "handle_button" function to each
+# We're watching the "FALLING" edge (transition from 3.3V to Ground) and
+# picking a generous bouncetime of 100ms to smooth out button presses.
+for pin in BUTTONS:
+    GPIO.add_event_detect(pin, GPIO.FALLING, handle_button, bouncetime=100)
+
+# Finally, since button handlers don't require a "while True" loop,
+# we pause the script to prevent it exiting immediately.
+signal.pause()
